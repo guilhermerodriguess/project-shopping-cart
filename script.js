b91031795c519dffb3dc73ab6c32132fdddf6ea0
Guilherme R <grsguiga@gmail.com>
2022-02-09 14:19:29 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
-  img.className = 'item__image';
+  img.className = 'item__image' ;
   img.src = imageSource;
   return img;
 }
